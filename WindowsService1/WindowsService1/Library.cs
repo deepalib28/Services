1ea8414106ec56cfa256b0c2a9d42a39654eb92a
dbhavsar <dbhavsar@tavisca.com>
2018-05-17 10:36:26 +0530

    added windows service

---
+++ WindowsService1/WindowsService1/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Log.txt", true);
+                //sw = new StreamWriter("d:\\Log.txt", true);
+                sw.WriteLine(DateTime.Now.ToString()+":"+Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+           
+        }
+    }
+}
---
+++ WindowsService1/WindowsService1/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer timer = null;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer();
+            timer.Interval = 30000;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Tick);
+            timer.Enabled = true;
+            Library.WriteErrorLog("Windows service started");
+
+        }
+
+        private void Timer_Tick(object sender, ElapsedEventArgs e)
+        {
+            Library.WriteErrorLog("job has been done successfully");
+
+        }
+
+        protected override void OnStop()
+        {
+            timer.Enabled =false;
+            Library.WriteErrorLog("Windows service stopped");
+        }
+    }
+}
